f56492f7b49c4f92c21fee55544029abbe99f7f3
SHITIANYU-hue <tianyu.s@outlook.com>
2022-03-12 21:56:50 +0800

    change

--- get_data.sh
+++ get_data.sh
@@ -11,12 +11,12 @@ cp -r map_files $PY_SITE_PACKAGE_PATH
 
 # step2: download Argoverse Motion Forecasting **v1.1** 
 # train + val + test
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_val_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_test_v1.1.tar.gz
-tar xvf forecasting_train_v1.1.tar.gz
-tar xvf forecasting_val_v1.1.tar.gz
-tar xvf forecasting_test_v1.1.tar.gz
+# wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
+# wget https://s3.amazonaws.com/argoai-argoverse/forecasting_val_v1.1.tar.gz
+# wget https://s3.amazonaws.com/argoai-argoverse/forecasting_test_v1.1.tar.gz
+# tar xvf forecasting_train_v1.1.tar.gz
+# tar xvf forecasting_val_v1.1.tar.gz
+# tar xvf forecasting_test_v1.1.tar.gz
 
 # step3: preprocess data to accelerate training
 
